0cbddd8f29889b924a8f8369c5306879134cae61
Rasha Ali <rashaali84@gmail.com>
2022-03-18 18:28:15 +0100

    Add project files.

---
+++ Data/TVShowsCatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TVShowsCatalog.Models;
+
+namespace TVShowsCatalog.Data
+{
+    public class TVShowsCatalogContext : DbContext
+    {
+        public TVShowsCatalogContext (DbContextOptions<TVShowsCatalogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TVShowsCatalog.Models.TvShow> TvShow { get; set; }
+    }
+}
